0b281d6ba7f4dc3cdc71a6bbd1a82142da76fd51
Edward Noel <ednoel59@gmail.com>
2022-03-16 10:31:36 +0100

    EntityManager + addDonnée

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -34,9 +37,52 @@ class SerieController extends AbstractController
     /**
      * @Route("/create", name="create")
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
+        //dump("yo");
+        dump($request);
         return $this->render('serie/create.html.twig', [
         ]);
+    }
+
+    /**
+     * @Route("/demo", name="em-demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        //Crée une instance
+        $serie = new Serie();
+
+        //hydrate toutes les propriété
+        $serie->setName('pif');
+        $serie->setBackdrop('dafsd');
+        $serie->setPoster('dafsd');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("-1 year"));
+        $serie->setLastAirDate(new \DateTime("-6 month"));
+        $serie->setGenres("drama");
+        $serie->setOverview("bla bla bla bla");
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus("Canceled");
+        $serie->setTmdbId(329432);
+
+        dump($serie);
+
+        //$entityManager = $this->getDoctrine()->getManager();
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        $serie->setGenres("comedy");
+        $entityManager->flush();
+
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
+
+        return $this->render('serie/create.html.twig');
     }
 }
